055444a18c7fbc4738fc64c1ccb66e0340c40186
WilliamEkedahl <167907468+WilliamEkedahl@users.noreply.github.com>
2024-10-08 19:25:49 +0200

    Update Valid.java

--- Valid.java
+++ Valid.java
@@ -1,10 +1,3 @@
-// PROG2 VT24, InlÃ¤mningsuppgift, del 2
-// Grupp 159
-// Max Lindberg mali7984
-// William Ekedahl wiek0904
-// Simon Lundqvist silu8199
-
-
 public class Valid {
     static boolean isValidInt(String time){
         try{
